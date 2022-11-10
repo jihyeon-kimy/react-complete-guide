e5b06c5ad0e869f3ed876549e6256849abef514e
jihyeon-kimy <jihyeon.rem@gmail.com>
2022-11-10 18:39:40 +0900

    03-practice-project-answer : Meals 컴포넌트 추가

--- 03-practice-project-answer/src/App.js
+++ 03-practice-project-answer/src/App.js
@@ -1,9 +1,15 @@
 import Header from "./components/Layout/Header";
+import Meals from "./components/Meals/Meals";
+import MealsSummary from "./components/Meals/MealsSummary";
 
 function App() {
   return (
     <>
       <Header />
+
+      <main>
+        <Meals />
+      </main>
     </>
   );
 }
---
+++ 03-practice-project-answer/src/components/Meals/Meals.js
@@ -0,0 +1,14 @@
+import React from "react";
+import AvailalbeMeals from "./AvailableMeals";
+import MealsSummary from "./MealsSummary";
+
+const Meals = () => {
+  return (
+    <>
+      <MealsSummary />
+      <AvailalbeMeals />
+    </>
+  );
+};
+
+export default Meals;
