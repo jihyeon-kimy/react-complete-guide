0567041ce2e85464a16d9476e7d22c711e34fe53
jihyeon-kimy <jihyeon.rem@gmail.com>
2022-11-10 18:10:40 +0900

    03-practice-project-answer : headerCartButton 컴포넌트 추가

--- 03-practice-project-answer/src/components/Layout/Header.js
+++ 03-practice-project-answer/src/components/Layout/Header.js
@@ -2,6 +2,7 @@ import React from "react";
 
 import mealsImage from "../../assets/meals.jpg";
 import classes from "./Header.module.css";
+import HeaderCartButton from "./HeaderCartButton";
 
 const Header = (props) => {
   return (
@@ -9,7 +10,7 @@ const Header = (props) => {
       {/* 전체를 한번에 작성하기 보다 큰 맥락에서 마크업하고, 디테일한 부분은 컴포넌트를 분리하여 작성한다. */}
       <header className={classes.header}>
         <h1>ReactMeals</h1>
-        <button>Cart</button>
+        <HeaderCartButton />
       </header>
       <div className={classes["main-image"]}>
         <img src={mealsImage} alt="A table full of delicious food!" />
