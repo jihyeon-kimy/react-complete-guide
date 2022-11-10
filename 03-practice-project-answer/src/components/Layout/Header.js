48bcdb67bc671ac1e26899f65e76abcd81475d4a
jihyeon-kimy <jihyeon.rem@gmail.com>
2022-11-10 17:53:18 +0900

    03-practice-project-answer : header 컴포넌트 추가

---
+++ 03-practice-project-answer/src/components/Layout/Header.js
@@ -0,0 +1,21 @@
+import React from "react";
+
+import mealsImage from "../../assets/meals.jpg";
+import classes from "./Header.module.css";
+
+const Header = (props) => {
+  return (
+    <>
+      {/* 전체를 한번에 작성하기 보다 큰 맥락에서 마크업하고, 디테일한 부분은 컴포넌트를 분리하여 작성한다. */}
+      <header className={classes.header}>
+        <h1>ReactMeals</h1>
+        <button>Cart</button>
+      </header>
+      <div className={classes["main-image"]}>
+        <img src={mealsImage} alt="A table full of delicious food!" />
+      </div>
+    </>
+  );
+};
+
+export default Header;
