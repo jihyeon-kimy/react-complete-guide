5ef74776d13121b3102938fe3ef132e19b4549a9
jihyeon-kimy <jihyeon.rem@gmail.com>
2022-11-11 10:32:55 +0900

    03-practice-project-answer : Input 공통 UI 컴포넌트 추가

---
+++ 03-practice-project-answer/src/components/UI/Input.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+import classes from "./Input.module.css";
+
+const Input = (props) => {
+  return (
+    <div className={classes.input}>
+      <label htmlFor={props.input.id}>{props.label}</label>
+      <input {...props.input} />
+    </div>
+  );
+};
+
+export default Input;
+
+// 요소에서 전개연산자 사용법
+// prop.input 객체에 있는 모든 키-값 쌍을 스프레드 문법으로 전개시켜주면, {type:'text'} => type='text 형태로 작동한다
