91493db0604a5c4daba4f03f359280d25a2cf5b5
jihyeon-kimy <jihyeon.rem@gmail.com>
2022-11-13 08:30:38 +0900

    05-starting-project : 로딩 및 데이터 state 처리 - 실제 데이터가 존재하는지 여부와 로딩 중인지 여부를 나누어 상태로 만든다. - 경우에 따른 처리 : 로딩중, 로딩완료+데이터O, 로딩완료+데이터X

--- 05-starting-project/src/App.js
+++ 05-starting-project/src/App.js
@@ -4,10 +4,13 @@ import MoviesList from "./components/MoviesList";
 import "./App.css";
 
 function App() {
+  // 영화의 상태를 가져오면 영화가 실제로 존재하는지 알 수 있다. 하지만 기다리는 중인지 알려주려면 또 다른 상태를 만들어야 한다.
   const [movies, setMovies] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
 
   // 1. async와 await
   async function fetchMovieHandler() {
+    setIsLoading(true); // 로딩이 시작할때 상태변화 발생
     const response = await fetch("https://swapi.dev/api/films/");
     const data = await response.json();
 
@@ -20,6 +23,7 @@ function App() {
       };
     });
     setMovies(transformedMovies);
+    setIsLoading(false);
   }
 
   // 2. then c체인 사용
@@ -47,7 +51,9 @@ function App() {
         <button onClick={fetchMovieHandler}>Fetch Movies</button>
       </section>
       <section>
-        <MoviesList movies={movies} />
+        {!isLoading && movies.length > 0 && <MoviesList movies={movies} />}
+        {!isLoading && movies.length === 0 && <p>Found no movies.</p>}
+        {isLoading && <p>Loading...</p>}
       </section>
     </React.Fragment>
   );
