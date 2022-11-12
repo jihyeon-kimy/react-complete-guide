576a5e7da7f4a6f6195ecb4308191a91537e515e
jihyeon-kimy <jihyeon.rem@gmail.com>
2022-11-12 16:30:17 +0900

    03-practice-project-answer : 장바구니 항목 수량이 변경될 경우 애니메이션 추가

--- 03-practice-project-answer/src/components/Layout/HeaderCartButton.js
+++ 03-practice-project-answer/src/components/Layout/HeaderCartButton.js
@@ -1,16 +1,44 @@
-import React from "react";
+import React, { useContext, useEffect, useState } from "react";
 
 import CartIcon from "../Cart/CartIcon";
 import classes from "./HeaderCartButton.module.css";
+import CartContext from "../../store/cart-context";
 
 const HeaderCartButton = (props) => {
+  const [btnIsHighlighted, setBtnIsHighlighted] = useState(false);
+  const cartCtx = useContext(CartContext);
+
+  const { items } = cartCtx;
+  const numberOfCartItems = items.reduce((curNumber, item) => {
+    return curNumber + item.amount;
+  }, 0);
+
+  const btnClasses = `${classes.button} ${btnIsHighlighted ? classes.bump : ""}`;
+
+  // useState추가 : 왜냐하면, 이 컴포넌트를 재평가하고 다시 렌더링해야 하니까. 애니메이션 클래스가 조건부로 추가될 때.
+  // cleanup 함수를 타이머를 추가한다 : 빠르게 여러 항목을 추가할 경우, 이전 타이머는 지워지고 확실히 새로운 타이머가 설정되기를 원한다.
+  useEffect(() => {
+    if (items.length === 0) {
+      return;
+    }
+    setBtnIsHighlighted(true);
+
+    const timer = setTimeout(() => {
+      setBtnIsHighlighted(false);
+    }, 300);
+
+    return () => {
+      clearTimeout(timer);
+    };
+  }, [items]);
+
   return (
-    <button className={classes.button}>
+    <button className={btnClasses} onClick={props.onClick}>
       <span className={classes.icon}>
         <CartIcon />
       </span>
       <span>Your Cart</span>
-      <span className={classes.badge}>3</span>
+      <span className={classes.badge}>{numberOfCartItems}</span>
     </button>
   );
 };
