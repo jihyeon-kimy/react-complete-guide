c32cac561d87cfa190dcb3f741ccad9e6ddfa643
jihyeon-kimy <jihyeon.rem@gmail.com>
2022-11-13 09:54:19 +0900

    05-starting-project : HTTP오류 처리 - fetch API는 에러 상태 코드를 기술적인 오류로 처리하지 않아서, (별도로 처리를 하지 않으면)가져오지 못한 데이터로 어떤 작업을 하려는 시점에 오류가 발생하게 된다. - 오류 상태 코드를 받았을 때, 처리를 하도록 상태코드에 맞는 오류를 만들어 전달해야 한다. - response.ok 여부만 확인해서 오류를 처리하는 간단히 로직을 작성하였다.

--- 05-starting-project/src/App.js
+++ 05-starting-project/src/App.js
@@ -4,29 +4,39 @@ import MoviesList from "./components/MoviesList";
 import "./App.css";
 
 function App() {
-  // 영화의 상태를 가져오면 영화가 실제로 존재하는지 알 수 있다. 하지만 기다리는 중인지 알려주려면 또 다른 상태를 만들어야 한다.
   const [movies, setMovies] = useState([]);
   const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState(null);
 
   // 1. async와 await
   async function fetchMovieHandler() {
     setIsLoading(true); // 로딩이 시작할때 상태변화 발생
-    const response = await fetch("https://swapi.dev/api/films/");
-    const data = await response.json();
+    setError(null); // 이전에 받았을 수도 있는 오류를 초기화
+    try {
+      const response = await fetch("https://swapi.dev/api/films/");
 
-    const transformedMovies = data.results.map((movieData) => {
-      return {
-        id: movieData.episode_id,
-        title: movieData.title,
-        openingText: movieData.opening_crawl,
-        releaseDate: movieData.release_date,
-      };
-    });
-    setMovies(transformedMovies);
+      if (!response.ok) {
+        throw new Error("Something went wrong");
+      } //fetch API는 실패하면 데이터를 보내지 않기 때문에 파싱 전에 오류처리를 해줘야 한다.
+
+      const data = await response.json();
+
+      const transformedMovies = data.results.map((movieData) => {
+        return {
+          id: movieData.episode_id,
+          title: movieData.title,
+          openingText: movieData.opening_crawl,
+          releaseDate: movieData.release_date,
+        };
+      });
+      setMovies(transformedMovies);
+    } catch (error) {
+      setError(error.message); // 에러 호출
+    }
     setIsLoading(false);
   }
 
-  // 2. then c체인 사용
+  // 2. then 체인 사용
   // function fetchMovieHandler() {
   //   fetch("https://swapi.dev/api/films/")
   //     .then((response) => {
@@ -45,16 +55,25 @@ function App() {
   //     });
   // }
 
+  let content = <p>Found no movies.</p>;
+
+  if (movies.length > 0) {
+    content = <MoviesList movies={movies} />;
+  }
+
+  if (error) {
+    content = <p>{error}</p>;
+  }
+  if (isLoading) {
+    content = <p>Loading...</p>;
+  }
+
   return (
     <React.Fragment>
       <section>
         <button onClick={fetchMovieHandler}>Fetch Movies</button>
       </section>
-      <section>
-        {!isLoading && movies.length > 0 && <MoviesList movies={movies} />}
-        {!isLoading && movies.length === 0 && <p>Found no movies.</p>}
-        {isLoading && <p>Loading...</p>}
-      </section>
+      <section>{content}</section>
     </React.Fragment>
   );
 }
