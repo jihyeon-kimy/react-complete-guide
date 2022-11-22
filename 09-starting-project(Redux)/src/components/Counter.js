516436c2b5ec9a0fdbb7f61332f0579f57ca3650
jihyeon-kimy <jihyeon.rem@gmail.com>
2022-11-22 12:03:16 +0900

    09-starting-project(Redux) : 리덕스 상태 추가(토글)

--- 09-starting-project(Redux)/src/components/Counter.js
+++ 09-starting-project(Redux)/src/components/Counter.js
@@ -5,6 +5,7 @@ import classes from "./Counter.module.css";
 const Counter = () => {
   const dispatch = useDispatch();
   const counter = useSelector((state) => state.counter);
+  const show = useSelector((state) => state.showCounter); // 리덕스 데이터가 업데이트될 때마다, 컴포넌트가 재평가된다.
 
   const incrementHandler = () => {
     dispatch({ type: "increment" });
@@ -18,18 +19,24 @@ const Counter = () => {
     dispatch({ type: "decrement" });
   };
 
-  const toggleCounterHandler = () => {};
+  const toggleCounterHandler = () => {
+    dispatch({ type: "toggle" });
+  };
 
   return (
     <main className={classes.counter}>
       <h1>Redux Counter</h1>
-      <div className={classes.value}>{counter}</div>
-      <div>
-        <button onClick={incrementHandler}>Increment</button>
-        <button onClick={increaseHandler}>Increment by 5</button>
+      {show && (
+        <>
+          <div className={classes.value}>{counter}</div>
+          <div>
+            <button onClick={incrementHandler}>Increment</button>
+            <button onClick={increaseHandler}>Increment by 5</button>
+            <button onClick={decrementHandler}>Decrement</button>
+          </div>
+        </>
+      )}
 
-        <button onClick={decrementHandler}>Decrement</button>
-      </div>
       <button onClick={toggleCounterHandler}>Toggle Counter</button>
     </main>
   );
--- 09-starting-project(Redux)/src/store/index.js
+++ 09-starting-project(Redux)/src/store/index.js
@@ -1,16 +1,38 @@
 import { createStore } from "redux";
 
-const counterReducer = (state = { counter: 0 }, action) => {
+const initialState = { counter: 0, showCounter: true };
+
+const counterReducer = (state = initialState, action) => {
   if (action.type === "increment") {
-    return { counter: state.counter + 1 };
+    return {
+      counter: state.counter + 1,
+      showCounter: state.showCounter,
+    };
+    // 전체 상태 객체를 리턴해야하니까 showCoounter도 함께 리턴하도록 바꿔주어야 한다.
+    // 리덕스는 기본 상태에 변화된걸 합치지 않는다. 대신 리턴한 것으로 기존 것을 대체한다(덮어쓴다).
+    // 기존 state를 변경해서는 안된다. state.counter++ return state 하면 안된다.
+    // 데이터를 업데이트할 때 기존 객체로 가서 절대 객체 속성이 조작되지 않도록 해야한다. 새로운 객체를 복사하고 생성하자
   }
 
   if (action.type === "increase") {
-    return { counter: state.counter + action.amount };
+    return {
+      counter: state.counter + action.amount,
+      showCounter: state.showCounter,
+    };
   }
 
   if (action.type === "decrement") {
-    return { counter: state.counter - 1 };
+    return {
+      counter: state.counter - 1,
+      showCounter: state.showCounter,
+    };
+  }
+
+  if (action.type === "toggle") {
+    return {
+      counter: state.counter,
+      showCounter: !state.showCounter,
+    };
   }
   return state;
 };
