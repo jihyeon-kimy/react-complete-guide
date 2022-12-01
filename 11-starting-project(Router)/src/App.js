7ed997763bfc3b6a4a32e5a25cc1f65214e86a84
jihyeon-kimy <jihyeon.rem@gmail.com>
2022-12-01 18:08:56 +0900

    11-starting-project(Router) : 리액트 라우터 경로 정의

--- 11-starting-project(Router)/src/App.js
+++ 11-starting-project(Router)/src/App.js
@@ -1,7 +1,21 @@
+import { Route } from "react-router-dom"; // Route는 실제 컴포넌트가 된다. 특정 경로를 정의한 다음 URL에서 경로가 활성화될 때 로드되어야 하는 리액트 컴포넌트를 정의하는 컴포넌트이다.
+import MainHeader from "./components/MainHeader";
+import Products from "./pages/Products";
+import Welcome from "./pages/Welcome";
+
 function App() {
   return (
     <div>
-      <h2>Let's get started!</h2>
+      <MainHeader />
+      <main>
+        {/* react 라우터는 url을 평가하고, url을 기반으로 올바른 컴포넌트를 랜더링 하는지 확인한다.  */}
+        <Route path="/welcome">
+          <Welcome />
+        </Route>
+        <Route path="/products">
+          <Products />
+        </Route>
+      </main>
     </div>
   );
 }
--- 11-starting-project(Router)/src/index.js
+++ 11-starting-project(Router)/src/index.js
@@ -1,7 +1,13 @@
-import ReactDOM from 'react-dom/client';
+import ReactDOM from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
 
-import './index.css';
-import App from './App';
+import "./index.css";
+import App from "./App";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(<App />);
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
+  // BrowserRouter 컴포넌트로 래핑을 하면, 리액트 라우터가 활성화 되고, 경로 정의와 같은 리액트 라우터 가능이 잠금해제된다.
+  <BrowserRouter>
+    <App />
+  </BrowserRouter>
+);
