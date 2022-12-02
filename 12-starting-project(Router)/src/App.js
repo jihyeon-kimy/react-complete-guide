2dda8e95e3b7008b372b2fe367f19c3a3230211c
jihyeon-kimy <jihyeon.rem@gmail.com>
2022-12-02 16:46:20 +0900

    12-starting-project(Router) : 라우트 세팅(route경로, navigation bar, layout)

--- 12-starting-project(Router)/src/App.js
+++ 12-starting-project(Router)/src/App.js
@@ -1,9 +1,27 @@
+import { Redirect, Route, Switch } from "react-router-dom";
+import Layout from "./components/layout/Layout";
+import AllQuotes from "./pages/AllQuotes";
+import NewQuotes from "./pages/NewQuote";
+import QuoteDetail from "./pages/QuoteDetail";
+
 function App() {
   return (
-    <div>
-
-    </div>
+    <Layout>
+      <Switch>
+        <Route path="/" exact>
+          <Redirect to="/quotes" />
+        </Route>
+        <Route path="/quotes" exact>
+          <AllQuotes />
+        </Route>
+        <Route path="/quotes/:quoteId">
+          <QuoteDetail />
+        </Route>
+        <Route path="/new-quote">
+          <NewQuotes />
+        </Route>
+      </Switch>
+    </Layout>
   );
 }
-
 export default App;
