4fc4617480608f8f21ff266311398c89d11ebc26
jihyeon-kimy <jihyeon.rem@gmail.com>
2022-12-03 16:59:20 +0900

    12-starting-project(Router) : Not found 페이지 추가 - 라우트에서 미리 설정하지 않은 경로에 접근할 경우 보여줄 페이지 추가

--- 12-starting-project(Router)/src/App.js
+++ 12-starting-project(Router)/src/App.js
@@ -2,6 +2,7 @@ import { Redirect, Route, Switch } from "react-router-dom";
 import Layout from "./components/layout/Layout";
 import AllQuotes from "./pages/AllQuotes";
 import NewQuotes from "./pages/NewQuote";
+import NotFound from "./pages/NotFound";
 import QuoteDetail from "./pages/QuoteDetail";
 
 function App() {
@@ -19,6 +20,9 @@ function App() {
         </Route>
         <Route path="/new-quote">
           <NewQuotes />
+        </Route>
+        <Route path="*">
+          <NotFound />
         </Route>
       </Switch>
     </Layout>
